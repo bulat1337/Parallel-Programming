f9ecf77512bafc6e814198c9ef5a55411890fc5d
bulat1337 <bulatmot@gmail.com>
2026-03-03 16:15:14 +0300

    move to task_1

---
+++ task_1/hello_mpi.cpp
@@ -0,0 +1,20 @@
+#include <mpi.h>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank = -1;
+    int size = 0;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    std::cout << "Hello World from rank " << rank
+              << " of " << size << std::endl;
+
+    MPI_Finalize();
+
+    return 0;
+}
